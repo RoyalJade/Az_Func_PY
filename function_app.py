4898369fe803a07eeafd89a53c4c6dc1e1783b22
Enrique Cabz <v-enriqueme@microsoft.com>
2023-06-19 16:01:17 +0000

    Changes to be committed: 	modified:   function_app.py

--- function_app.py
+++ function_app.py
@@ -7,7 +7,7 @@ app = func.FunctionApp()
 
 # Get started by running the following code to create a function using a HTTP trigger.
 
-@app.function_name(name="HttpTrigger1")
+@app.function_name(name="FncTeste")
 @app.route(route="hello", auth_level = func.AuthLevel.ANONYMOUS)
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
      logging.info('Python HTTP trigger function processed a request.')
@@ -25,6 +25,6 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
      else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully (?). Pass a name in the query string or in the request body for a personalized response!!.",
              status_code=200
         )
